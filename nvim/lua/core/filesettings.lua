f1df3ce28e256b7dda409fa083cf58ecc1ad1f34
callmemikhail <boykomichael@proton.me>
2025-04-26 22:52:09 +0300

    spellcheck

--- nvim/lua/core/filesettings.lua
+++ nvim/lua/core/filesettings.lua
@@ -1,4 +1,5 @@
 --[File-view]--
 	opt.fixeol = true -- file extension
+    optl.spell = true -- local spell check
 	o.encoding = 'utf-8' -- view file encoding
 	opt.fileformat = "unix" -- unix file format	
--- nvim/lua/core/vars.lua
+++ nvim/lua/core/vars.lua
@@ -6,4 +6,5 @@
     cmd = vim.cmd
     opt = vim.opt
     keymap = vim.keymap
+    optl = vim.opt_local
     map = vim.api.nvim_set_keymap 
